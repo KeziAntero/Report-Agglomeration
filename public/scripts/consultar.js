b543b672bffe301d00de53a9ede15d629f8cbdc5
KeziAntero <keziantero@gmail.com>
2020-11-01 01:16:14 -0300

    Inclusão da Api do Google maps

--- public/scripts/consultar.js
+++ public/scripts/consultar.js
@@ -1,23 +1,55 @@
-const map = L.map('mapid').setView([-6.47926,-35.4348], 15);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
-.addTo(map);
-
-const icon = L.icon({
-    iconUrl: "./public/images/location.svg",
-    iconSize: [58, 48],
-    iconAnchor: [29,68],
-    popupAnchor: [170, 2]
-})
-
-const popup = L.popup({
-    closeButton: false,
-    className: "map-popup",
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Av. Assis Chateaubriand Data do registro: 20/10/2020 Hora do registro: 02:59 Nº estimado de pessoas: 20 Pessoas usando máscara: Nenhuma Distanciamento sendo respeitado: Não Observações: som alto <a href="indices.html?id=1" class="add-aglomeracao"> <img src="./public/images/arrow-white.svg"> </a>')
-
-L
-.marker([-6.47926,-35.4348], { icon })
-.addTo(map)
-.bindPopup(popup)
+ // This sample uses the Place Autocomplete widget to allow the user to search
+      // for and select a place. The sample then displays an info window containing
+      // the place ID and other information about the place that the user has
+      // selected.
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+      function initMap() {
+        const map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: -33.8688, lng: 151.2195 },
+          zoom: 15,
+          disableDefaultUI: true,
+        });
+        const input = document.getElementById("pac-input");
+        const autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo("bounds", map);
+        // Specify just the place data fields that you need.
+        autocomplete.setFields(["place_id", "geometry", "name"]);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        const infowindow = new google.maps.InfoWindow();
+        const infowindowContent = document.getElementById("infowindow-content");
+        infowindow.setContent(infowindowContent);
+        const marker = new google.maps.Marker({ map: map });
+        marker.addListener("click", () => {
+          infowindow.open(map, marker);
+        });
+        autocomplete.addListener("place_changed", () => {
+          infowindow.close();
+          const place = autocomplete.getPlace();
+    
+          if (!place.geometry) {
+            return;
+          }
+    
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+          }
+          // Set the position of the marker using the place ID and location.
+          marker.setPlace({
+            placeId: place.place_id,
+            location: place.geometry.location,
+          });
+          marker.setVisible(true);
+          infowindowContent.children.namedItem("place-name").textContent =
+            place.name;
+          infowindowContent.children.namedItem("place-id").textContent =
+            place.place_id;
+          infowindowContent.children.namedItem("place-address").textContent =
+            place.formatted_address;
+          infowindow.open(map, marker);
+        });
+      }
