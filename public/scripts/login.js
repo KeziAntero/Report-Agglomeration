7a405323d6f43f813c5da01b01cfd59856bd388b
KeziAntero <keziantero@gmail.com>
2020-11-09 04:22:44 -0300

    incluindo autenticação com facebook

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,26 +1,115 @@
-function onSignIn(response) {
-        
-    // Conseguindo as informações do seu usuário:
-    var perfil = response.getBasicProfile();
-
-    // Conseguindo o ID do Usuário
-    var userID = perfil.getId();
-
-    // Conseguindo o Nome do Usuário
-    var userName = perfil.getName();
-
-    // Conseguindo o E-mail do Usuário
-    var userEmail = perfil.getEmail();
-
-    // Conseguindo a URL da Foto do Perfil
-    var userPicture = perfil.getImageUrl();
-
-    document.getElementById('user-photo').src = userPicture;
-    document.getElementById('user-name').innerText = userName;
-    document.getElementById('user-email').innerText = userEmail;
+//Login com Facebook
+window.fbAsyncInit = function() {
+    // FB Configuração e instalação do JavaScript SDK
+    FB.init({
+      appId      : '1539259386284405', // FB App ID
+      cookie     : true,  // habilitando os cookies para permitir que o servidor acesse a sessão
+      xfbml      : true,  // analisando os plug-ins da página
+      version    : 'v3.2' // use graph api version 2.8
+    });
     
-    // Recebendo o TOKEN que você usará nas demais requisições à API:
-    var LoR = response.getAuthResponse().id_token;
-    console.log("~ le Tolkien: " + LoR);
-   
+    // Verificando se o usuário já está logado
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            //exibindo dados do usuário
+            getFbUserData();
+        }
+    });
 };
+
+// Carregando o SDK do JavaScript de forma assíncrona
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// login com JavaScript SDK
+function fbLogin() {
+    FB.login(function (response) {
+        if (response.authResponse) {
+            // Obtendo e exibindo os dados do perfil do usuário
+            getFbUserData();
+        } else {
+            document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
+        }
+    }, {scope: 'email'});
+}
+
+// Buscando os dados do perfil do usuário de facebook
+function getFbUserData(){
+    FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture'},
+    function (response) {
+        document.getElementById('fbLink').setAttribute("onclick","fbLogout()");
+        document.getElementById('fbLink').innerHTML = 'Logout from Facebook';
+        document.getElementById('status').innerHTML = '<p>Thanks for logging in, ' + response.first_name + '!</p>';
+        document.getElementById('userData').innerHTML = '<h2>Facebook Profile Details</h2><p><img src="'+response.picture.data.url+'"/></p><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>FB Profile:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
+    });
+}
+
+// Logout from facebook
+function fbLogout() {
+    FB.logout(function() {
+        document.getElementById('fbLink').setAttribute("onclick","fbLogin()");
+        document.getElementById('fbLink').innerHTML = '<img src="publicl/images/fb-login-btn.png"/>';
+        document.getElementById('userData').innerHTML = '';
+        document.getElementById('status').innerHTML = '<p>You have successfully logout from Facebook.</p>';
+    });
+}
+
+//***********************************************************************************************//
+
+//Login com o Goggle
+// Render Google Sign-in button
+function renderButton() {
+    gapi.signin2.render('gSignIn', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
+
+// Sign-in success callback
+function onSuccess(googleUser) {
+    // Get the Google profile data (basic)
+    //var profile = googleUser.getBasicProfile();
+    
+    // Retrieve the Google account data
+    gapi.client.load('oauth2', 'v2', function () {
+        var request = gapi.client.oauth2.userinfo.get({
+            'userId': 'me'
+        });
+        request.execute(function (resp) {
+            // Display the user details
+            var profileHTML = '<h3>Welcome '+resp.given_name+'! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></h3>';
+            profileHTML += '<img src="'+resp.picture+'"/><p><b>Google ID: </b>'+resp.id+'</p><p><b>Name: </b>'+resp.name+'</p><p><b>Email: </b>'+resp.email+'</p><p><b>Gender: </b>'+resp.gender+'</p><p><b>Locale: </b>'+resp.locale+'</p><p><b>Google Profile:</b> <a target="_blank" href="'+resp.link+'">click to view profile</a></p>';
+            document.getElementsByClassName("userContent")[0].innerHTML = profileHTML;
+            
+            document.getElementById("gSignIn").style.display = "none";
+            document.getElementsByClassName("userContent")[0].style.display = "block";
+        });
+    });
+}
+
+// Sign-in failure callback
+function onFailure(error) {
+    alert(error);
+}
+
+// Sign out the user
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        document.getElementsByClassName("userContent")[0].innerHTML = '';
+        document.getElementsByClassName("userContent")[0].style.display = "none";
+        document.getElementById("gSignIn").style.display = "block";
+    });
+    
+    auth2.disconnect();
+}
