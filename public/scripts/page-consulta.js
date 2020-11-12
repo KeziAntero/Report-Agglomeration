a34f1df38f24a0425d8e35798f0f660c443c06e0
KeziAntero <keziantero@gmail.com>
2020-11-12 01:04:56 -0300

    Inclusão de campo de input de busca no mapa

--- public/scripts/page-consulta.js
+++ public/scripts/page-consulta.js
@@ -1,47 +1,24 @@
-if ('geolocation' in navigator) {
-    
-    navigator.geolocation.getCurrentPosition(position => {
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
-      
-      document.getElementById('latitude').textContent = lat;
-      document.getElementById('longitude').textContent = lon;
 
-      const mapid = L.map('mapid').setView([lat, lon], 15);
-      const attribution =
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-      const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-      const tiles = L.tileLayer(tileUrl, { attribution });
-      tiles.addTo(mapid);
-      
 
-      
-      const icon = L.icon({
-        iconUrl: "./public/images/mark-green.png",
-        iconSize: [58, 68],
-        iconAnchor: [29,68],
-        popupAnchor: [170, 2]
-    })
-    
-    const popup = L.popup({
-        closeButton: false,
-        className: "map-popup",
-        minWidth: 240,
-        minHeight: 240
-    }).setContent('Indices <a href="indices.html?id=1"> <img src="./public/images/arrow-white.svg"> </a>')
-     
-    L
-    .marker([lat, lon], { icon })
-    .addTo(mapid)
-    .bindPopup(popup)
-    
+var map = L.map('mapid').setView([-9.5510128, -49.62864725], 4);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+  var results = L.layerGroup().addTo(map);
+
+  searchControl.on('results', function (data) {
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+    }
+    earthquakes.bindPopup(function (layer) {
+      return L.Util.template('<p>Earthquake <strong>{name}</strong> occured on {mo}/{dy}/{year_}.  It had a magnitude of {magnitude}.</p>', layer.feature.properties);
     });
-    
-  } else {
-    document.getElementById('geolocation not available');
-  }
-
-
-
+  });
 
 
