c2c2e7927e3bb7725e4d5170648fb830b75fe934
KeziAntero <keziantero@gmail.com>
2020-12-21 15:54:20 -0300

    Correção da tabela das maiores aglomeração

--- public/scripts/table-top-index.js
+++ public/scripts/table-top-index.js
@@ -5,23 +5,24 @@ var autoTopFunction = setInterval(getTopReportes,5000);
 var xhr = new XMLHttpRequest();
 
 xhr.addEventListener("load", function () {
-    var table = document.querySelector(".table");
+    var tbody = document.querySelector(".tbody");
     var report = JSON.parse(xhr.responseText);
     for (i = 0; i < 5; i++) {
-        var tr = criarTr(report[i]);
-        table.appendChild(tr);
+        var tr = criarTr(report[i],i+1);
+        tbody.appendChild(tr);
     }
 });
 
 function getTopReportes() {
-    var tbd = document.querySelector(".table");
+    var tbd = document.querySelector(".tbody");
     limparTable(tbd);
     xhr.open("GET", urlTopWS);
     xhr.send();
 }
 
-function criarTr(dado){
+function criarTr(dado,n){
     var tr = document.createElement("tr");
+    tr.appendChild(criarTd(n,"classificacao"))
     tr.appendChild(criarTd(dado.nome,"nome"));
     tr.appendChild(criarTd(dado.quantidade,"quantidade"));
 
@@ -38,8 +39,8 @@ function criarTd(conteudo, classe){
 
 function limparTable(old_tbd) {
     var table = document.querySelector("table");
-    var n_tbd = document.createElement("table");
-    n_tbd.classList.add("table");
+    var n_tbd = document.createElement("tbody");
+    n_tbd.classList.add("tbody");
     table.removeChild(old_tbd);
     table.appendChild(n_tbd);
 }
