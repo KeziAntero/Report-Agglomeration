b039a822cb827270a129278c9333966ab5dacb21
RapadureX <54689354+RapadureX@users.noreply.github.com>
2021-01-04 09:21:42 -0300

    Atualizando o link do action do form

--- add-aglomeracao.php
+++ add-aglomeracao.php
@@ -47,7 +47,7 @@
       </aside>
 
       <main class="animate-appear with-sidebar">
-        <form action="http://7.156.46.82/webservice/reporte_consulta_aglomeracao/" method="POST">
+        <form action="http://ws-reporte-consulta.brazilsouth.cloudapp.azure.com/" method="POST">
           <fieldset>
             <legend>Adicionar aglomeração</legend>
 
